7e5fab67f12cb8a68539e427bbd2177f226914b3
bishaladhikari56 <bishaladhikari56@gmail.com>
2022-11-05 12:57:01 -0700

    add projects

---
+++ Kafka-And-SFTP/src/main/java/dev/simplesolution/sftp/KafkaAndSftpApplication.java
@@ -0,0 +1,13 @@
+package dev.simplesolution.sftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAndSftpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAndSftpApplication.class, args);
+	}
+
+}
